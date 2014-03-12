11934a67f943a7e8edb1d6572ff7f4e209fdcc19
Xabier Larrakoetxea <slok69@gmail.com>
2014-03-12 23:42:07 +0100

    Statsd config update

--- statsd/statsd_config.js
+++ statsd/statsd_config.js
@@ -1,5 +1,5 @@
 {
-  graphitePort: 2003
+  graphitePort: parseInt(process.env.ENDPOINT_PORT_2003_TCP_PORT) || 2003
 , graphiteHost: process.env.ENDPOINT_PORT_2003_TCP_ADDR || "graphite.sharestack.org"
 , port: 8125
 , backends: [ "./backends/graphite" ]
